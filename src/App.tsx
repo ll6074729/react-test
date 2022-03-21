7b62d253bb76cdc9e587655194a5595928adefed
刘理 <liuli@liulideMacBook-Pro.local>
2022-03-21 13:06:42 +0800

    3.13 异步请求

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,50 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import React, { useState } from 'react'
+import logo from './assets/images/logo.svg'
 // import './App.css'
 import Robot from './components/Robot'
 import robots from './mock/robots.json'
 import styles from './App.module.css'
+import ShoppingCart from './components/ShoppingCart'
 
+interface Props{
 
-function App() {
-  return (
+}
+interface State {
+  robotGallery: any[]
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      robotGallery: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then((data) => this.setState({robotGallery: data}))
+  }
+
+  render(): React.ReactNode {
+      return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo}></img>
+        <h1>罗伯特机器人吊炸天</h1>
+      </div>
+      <ShoppingCart />
       <div className={styles.robotList}>
       {
-        robots.map( r => <Robot id={r.id} name={r.name} email={r.email}> </Robot>)
+        this.state.robotGallery.map( (r, i) => <Robot id={r.id} name={r.name} email={r.email} key={i}> </Robot>)
       } 
    </div>
     </div>
     
   )
+  }
+  
 }
 
 export default App
---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from './ShoppingCart.module.css'
+import { FiShoppingCart} from 'react-icons/fi'
+
+interface Props {
+
+}
+
+interface State {
+  isOpen: boolean
+}
+
+class ShoppingCart extends React.Component<Props, State>{
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log('🚀 ~ file: ShoppingCart.tsx ~ line 21 ~ ShoppingCart ~ handleClick ~ e', e)
+
+    if ((e.target as HTMLElement).nodeName === "SPAN") {
+      this.setState({ isOpen: !this.state.isOpen });
+
+    }
+  }
+  render(): React.ReactNode {
+    return (
+      <div className={styles.cartContainer}>
+        <button className={styles.button} onClick={this.handleClick }>
+          <FiShoppingCart></FiShoppingCart>
+          <span>购物车(2)</span>
+        </button>
+        <div className={styles.cartDropDown} style={{display: this.state.isOpen ? 'block': 'none'}}>
+          <ul>
+            <li>
+              robot 1
+            </li>
+            <li>
+              robot 2
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ShoppingCart
