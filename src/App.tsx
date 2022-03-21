aa280c3cf27e43fae7be229e8c8830582a4cb76e
刘理 <liuli@liulideMacBook-Pro.local>
2022-03-21 10:11:24 +0800

    3.6配置css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+// import './App.css'
 import Robot from './components/Robot'
 import robots from './mock/robots.json'
+import styles from './App.module.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <ul>
+    <div className={styles.app}>
+      <div className={styles.robotList}>
       {
         robots.map( r => <Robot id={r.id} name={r.name} email={r.email}> </Robot>)
       } 
-   </ul>
+   </div>
+    </div>
+    
   )
 }
 
